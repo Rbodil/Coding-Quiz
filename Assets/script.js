dccbc201b2f87ca38124322c4ef03b269f511263
Reagan Bodily <reaganbodily@gmail.com>
2022-02-13 20:17:02 -0700

    css and html framing completed

---
+++ Assets/script.js
@@ -0,0 +1,51 @@
+var timerEl = document.getElementById('#timer');
+
+var javascriptBtn = document.getElementById("#javascript-button");
+var cssBtn = document.getElementById("#css-button")
+var htmlBtn = document.getElementById("#html-button")
+var domBtn = document.getElementById("#dom-button")
+var apiBtn = document.getElementById("#api-button")
+var takeQuizBtn = document.getElementById("#select-subjects");
+
+var questionEl = document.getElementById('#question-header');
+
+var answerAbtn = document.getElementById('#answer-A');
+var answerBbtn = document.getElementById('#answer-B');
+var answerCbtn = document.getElementById('#answer-C');
+var answerDbtn = document.getElementById('#answer-D');
+var submitAnsBtn = document.getElementById('#submit-answer');
+
+
+
+
+
+
+
+
+
+ /*how do I randomize multiple choice answers? how do I have individual arrays for each question?
+ 
+ */
+
+
+
+
+
+
+// take quiz function needs to include multiple subjects with randomized answers within respective arrays, require at least one button selection, make buttons visible, and start timer
+
+// start timer function needs to be 15 minutes per array
+
+// select quizes function needs to have at least one input selected before running take quiz function: 
+// event.preventDefault(); , if x===true &| y===true &| z===true etc, return (testQuestions)
+// testQuestions = testSelected.array; if (x===true) testQuestions = testQuestions + xarray;
+
+
+
+
+
+
+// Time remaining in header
+// how to submit quiz on last question
+// how to keep track of right and wrong answers setElement/getElement
+// add arrays w/ 15 values per 
